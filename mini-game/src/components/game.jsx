158b22b0edf55f867de3fa8228948da89aafea31
s-weberg <sandraweberg90@gmail.com>
2025-05-17 17:06:12 +0200

    updated game and css

--- mini-game/src/components/game.jsx
+++ mini-game/src/components/game.jsx
@@ -1,14 +1,40 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import './App.css'
+import { useEffect, useState } from 'react';
+import '../App';
 
-  function iAmBored() {
-    const [questions, setQuestions] = useState([]);
-    const [index, setIndex] = useState(0);
-    const [score, setScore] = useState(0);
+function IAmBored() {
+  const [activity, setActivity] = useState(null);
+  const [hasFetched, setHasFetched] = useState(false);
+
+ const fetchData = async () => {
+  try {
+    setHasFetched(true);
+    const response = await fetch('https://dog.ceo/api/breeds/image/random'); 
+    const data = await response.json();
+    setActivity(data.activity);
+  } catch (error) {
+    console.error("Error", error);
   }
+};
 
+  useEffect(() => {
+    if (!hasFetched) {
+      fetchData();
+    }
+  }, [hasFetched]);
 
+  return (
+    <div>
+        <div className='container'>
 
+            <h1 className='title'>Game</h1>
+      {activity ? (
+        <p className="info">{activity.activity}</p>
+      ) : (
+        <p className='info'>Loading...</p>
+      )}
+      </div>
+    </div>
+  );
+}
 
-  export default iAmBored;
+export default IAmBored;
