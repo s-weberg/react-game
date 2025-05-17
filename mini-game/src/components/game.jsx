788ff5c63f4e1689ff9f36dc42f74ef30fdb29c2
s-weberg <sandraweberg90@gmail.com>
2025-05-17 16:31:50 +0200

    first commit

---
+++ mini-game/src/components/game.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import './App.css'
+
+  function iAmBored() {
+    const [questions, setQuestions] = useState([]);
+    const [index, setIndex] = useState(0);
+    const [score, setScore] = useState(0);
+  }
+
+
+
+
+  export default iAmBored;
